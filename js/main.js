e67ecd97b77497ad041b10738ed2e536c1b8d950
daladn0 <markkostevychx@gmail.com>
2020-05-04 19:57:49 +0300

    added modal window

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
+
+    // Tabs
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -33,5 +35,65 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Timer
+    let deadline = '2021-01-01';
+    
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse( new Date() ),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                total: t,
+                hours,
+                minutes,
+                seconds,
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+        
+            if( t.total <= 0 ) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
+
+    // Modal widnow
+
+    let  more = document.querySelector('.more'),
+         overlay = document.querySelector('.overlay'),
+         close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', windowShow);
+    close.addEventListener('click', windowClose);
+
+    function windowShow() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function windowClose() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    }
 
 });
