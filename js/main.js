33c7ca5438cf29bb0b861726db6307f181fcecbe
daladn0 <markkostevychx@gmail.com>
2020-05-11 23:14:07 +0300

    added calcualtor

--- js/main.js
+++ js/main.js
@@ -192,4 +192,51 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+
+    // Calculator
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.textContent = '0';
+
+    persons.addEventListener('change', function(){
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if(restDays.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    restDays.addEventListener('change', function(){
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if(daysSum.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if(daysSum.value == '' || restDays.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            let a = total;
+            totalValue.textContent = a * this.options[this.selectedIndex].value;
+        }
+
+
+    });
+
 });
